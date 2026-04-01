fc07e99bd966da2c89d560a2e8a5939f53ad59d0
Victor <victoroliveira.3002@gmail.com>
2026-04-01 00:55:21 -0300

    fix: adjust safeLimit in refreshMood endpoint to improve performance

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -99,7 +99,7 @@ export class UserController {
     @Get('refreshMood')
     @UseGuards(JwtAuthGuard)
     async RefreshMoodUser(@Req() req: MRequest, @Query('studioId') studioId?: string) {
-        const safeLimit = 50
+        const safeLimit = 33
         return await this.userService.RefreshMoodUserToday(req.user!.id, studioId, safeLimit);
     }
 
